0cf2a34a979ebf2479a4534432ac131b0aa5ed0a
maciejkula <maciej.kula@gmail.com>
2017-06-16 22:36:07 +0100

    Switch miniconda on linux.

--- .travis/install.sh
+++ .travis/install.sh
@@ -3,7 +3,11 @@
 set -e
 
 # Download and install conda
-wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O ~/miniconda.sh
+if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+    wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O ~/miniconda.sh
+else
+    wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
+fi
 bash ~/miniconda.sh -b -p $HOME/miniconda
 export PATH="$HOME/miniconda/bin:$PATH"
 rm ~/miniconda.sh
