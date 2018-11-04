26e172f7a7e75e9a7f017325ce390307d7bd4e28
NegatioN <joakimrishaug@gmail.com>
2018-11-04 12:52:48 +0100

    Add test to ensure model is instanciated on load()

--- tests/test_persist.py
+++ tests/test_persist.py
@@ -17,3 +17,21 @@ def test_all_params_persisted():
         ob = getattr(model, x)
         if not callable(ob) and not x.startswith('__'):
             assert x in saved_model_params
+
+def test_model_populated():
+    # Train and persist a model
+    data = fetch_movielens(min_rating=5.0)
+    model = LightFM(loss='warp')
+    model.fit(data['train'], epochs=5, num_threads=4)
+    model.save('./test.npz')
+
+    # Load a model onto an uninstanciated object
+    model = LightFM(loss='warp')
+
+    assert model.item_embeddings == None
+    assert model.user_embeddings == None
+
+    model.load('./test.npz')
+
+    assert model.item_embeddings.any()
+    assert model.user_embeddings.any()
