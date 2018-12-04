23efc0cf4da51d7042e5c59612df0725180fd7ff
NegatioN <joakimrishaug@gmail.com>
2018-12-04 13:50:59 +0100

    Add test to confirm performance of model is identical after model loaded from file

--- tests/test_persist.py
+++ tests/test_persist.py
@@ -3,10 +3,29 @@ import pytest
 import numpy as np
 import os
 
+from sklearn.metrics import roc_auc_score
+
 from lightfm import LightFM
 from lightfm.datasets import fetch_movielens
 
 TEST_FILE_PATH = "./tests/test.npz"
+
+
+def _binarize(dataset):
+
+    positives = dataset.data >= 4.0
+    dataset.data[positives] = 1.0
+    dataset.data[np.logical_not(positives)] = -1.0
+
+    return dataset
+
+
+def cleanup():
+    os.remove(TEST_FILE_PATH)
+
+
+movielens = fetch_movielens()
+train, test = _binarize(movielens["train"]), _binarize(movielens["test"])
 
 
 def test_all_params_persisted():
@@ -23,8 +42,7 @@ def test_all_params_persisted():
         if not callable(ob) and not x.startswith("__"):
             assert x in saved_model_params
 
-    # Clean up
-    os.remove(TEST_FILE_PATH)
+    cleanup()
 
 
 def test_model_populated():
@@ -45,5 +63,40 @@ def test_model_populated():
     assert model.item_embeddings.any()
     assert model.user_embeddings.any()
 
-    # Clean up
-    os.remove(TEST_FILE_PATH)
+    cleanup()
+
+
+def test_model_performance():
+    # Train and persist a model
+    model = LightFM(random_state=10)
+    model.fit_partial(train, epochs=10, num_threads=4)
+    model.save(TEST_FILE_PATH)
+
+    train_predictions = model.predict(train.row, train.col)
+    test_predictions = model.predict(test.row, test.col)
+
+    trn_pred = roc_auc_score(train.data, train_predictions)
+    tst_pred = roc_auc_score(test.data, test_predictions)
+    assert trn_pred > 0.84
+    assert tst_pred > 0.76
+
+    # Performance is worse when trained for 1 epoch
+    model = LightFM()
+    model.fit_partial(train, epochs=1, num_threads=4)
+
+    train_predictions = model.predict(train.row, train.col)
+    test_predictions = model.predict(test.row, test.col)
+
+    assert roc_auc_score(train.data, train_predictions) < 0.84
+    assert roc_auc_score(test.data, test_predictions) < 0.76
+
+    # Performance is same as previous when loaded from disk
+    model.load(TEST_FILE_PATH)
+
+    train_predictions = model.predict(train.row, train.col)
+    test_predictions = model.predict(test.row, test.col)
+
+    assert roc_auc_score(train.data, train_predictions) == trn_pred
+    assert roc_auc_score(test.data, test_predictions) == tst_pred
+
+    cleanup()
