29683581d745b1e0d3e43f74b30ad4dbf3d7058f
NegatioN <joakimrishaug@gmail.com>
2018-12-04 14:15:27 +0100

    Don't load dataset multiple times

--- tests/test_persist.py
+++ tests/test_persist.py
@@ -29,16 +29,15 @@ train, test = _binarize(movielens["train"]), _binarize(movielens["test"])
 
 
 def test_all_params_persisted():
-    # Train and persist a model
-    data = fetch_movielens(min_rating=5.0)
     model = LightFM(loss="warp")
-    model.fit(data["train"], epochs=5, num_threads=4)
+    model.fit(movielens["train"], epochs=1, num_threads=4)
     model.save(TEST_FILE_PATH)
 
     # Load and confirm all model params are present.
     saved_model_params = list(np.load(TEST_FILE_PATH).keys())
     for x in dir(model):
         ob = getattr(model, x)
+        # We don't need to persist model functions, or magic variables of the model.
         if not callable(ob) and not x.startswith("__"):
             assert x in saved_model_params
 
@@ -46,10 +45,8 @@ def test_all_params_persisted():
 
 
 def test_model_populated():
-    # Train and persist a model
-    data = fetch_movielens(min_rating=5.0)
     model = LightFM(loss="warp")
-    model.fit(data["train"], epochs=5, num_threads=4)
+    model.fit(movielens["train"], epochs=1, num_threads=4)
     model.save(TEST_FILE_PATH)
 
     # Load a model onto an uninstanciated object
